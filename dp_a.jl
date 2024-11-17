5277e38fd955006b2b236be83b2fc16ea81d68b2
1s22s1 <1s22s1@users.noreply.github.com>
2024-11-18 07:41:34 +0900

    :up:

---
+++ dp_a.jl
@@ -0,0 +1,22 @@
+using Printf
+
+function main()
+    n = parseint()
+    hn = parseints()
+
+    dp = [0]
+    push!(dp, abs(hn[2] - hn[1]))
+
+    for i ∈ 3:n
+        push!(dp, min(dp[i-1]+abs(hn[i]-hn[i-1]), dp[i-2]+abs(hn[i]-hn[i-2])))
+    end
+
+    println(dp[end])
+end
+
+parseint() = readline() |> x -> parse(Int, x)
+parsestring() = readline()
+parseints() = readline() |> split |> x -> parse.(Int, x)
+parsestrings() = readline() |> split
+
+main()
