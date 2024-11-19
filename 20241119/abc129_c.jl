ac99c0dc1765e83f97c2622ed94585ef39a32d86
1s22s1 <1s22s1@users.noreply.github.com>
2024-11-19 22:10:18 +0900

    :up:

--- 20241119/abc129_c.jl
+++ 20241119/abc129_c.jl
@@ -1,31 +1,32 @@
 using Printf
 
 function main()
+    mod = 1000000007
+
     n, m = parseints()
     am = [parseint() for _ ∈ 1:m]
+    # am .+= 1
 
-    dp = [1]
+    dp = fill(0, n+1)
+    dp[1] = 1
 
-    1 ∈ am ? push!(dp, 0) : push!(dp, 1)
-
-    for i ∈ 3:n+1
-        value = 0
-        @show i, i - 1, i - 2
-        if i - 1 ∉ am
-            value += dp[i-1]
-        end
-
-        if i - 2 ∉ am
-            value += dp[i-2]
-        end
-
-        push!(dp, value)
+    if 2 ∉ am
+        dp[2] = 1
     end
 
-    @show dp
-    result = dp[end]
+    for i ∈ 3:n+1
+        if i-1 ∉ am
+            dp[i] += dp[i-1]
+        end
 
-    println(result)
+        if i-2 ∉ am
+            dp[i] += dp[i-2]
+        end
+
+        dp[i] %= mod
+    end
+
+    println(dp[end])
 end
 
 parseint() = readline() |> x -> parse(Int, x)
